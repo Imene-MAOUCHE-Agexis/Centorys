21fac668510319182548cbeafc33ba852ca1e582
imenemch <maoucheime0675@gmail.com>
2024-04-24 16:57:33 +0200

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="" />
+        <ul>
+            <li>Actualités</li>
+            <li>Besoins</li>
+            <li>Solutions</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
